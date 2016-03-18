ee3c1743bc5a918db1c6d62052d4946969e09567
Chester Law <chesterlaw@me.com>
2016-03-18 23:43:32 +0900

    Add white-out and integrate with modal functions

--- app/assets/javascripts/possessions.js
+++ app/assets/javascripts/possessions.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
   $('.got-rid').click(function() {
     $('.modal--got-rid').toggle();
+    $('.white-out').toggle();
   });
+
   $('.modal__close-modal').click(function() {
     $(this).parents('.modal').hide();
+    $('.white-out').hide();
+  });
+
+  $('.white-out').click(function() {
+    $('.modal').hide();
+    $(this).hide();
   });
 });
 
