c05b125a5412f87288dc847d3ece620109459f28
Chester Law <chesterlaw@me.com>
2016-03-18 22:59:05 +0900

    Make close-modal ITCSS compliant

--- app/assets/javascripts/possessions.js
+++ app/assets/javascripts/possessions.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $('.got-rid').click(function() {
     $('.modal--got-rid').toggle();
   });
+  $('.modal__close-modal').click(function() {
+    $(this).parents('.modal').hide();
+  });
 });
 
