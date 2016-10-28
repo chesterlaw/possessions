b6673355cc34fa48af014adef05623e0e88423eb
Chester Law <chesterlaw@me.com>
2016-10-28 16:44:56 +0900

    Set default for got_rid_of

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028073603) do
+ActiveRecord::Schema.define(version: 20161028074018) do
 
   create_table "possessions", force: :cascade do |t|
     t.string   "name"
@@ -19,13 +19,13 @@ ActiveRecord::Schema.define(version: 20161028073603) do
     t.string   "image_url"
     t.datetime "got_rid_of_at"
     t.text     "got_rid_of_reason"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.integer  "user_id"
     t.integer  "month_purchased"
     t.integer  "year_purchased"
     t.string   "amazon_link"
-    t.boolean  "got_rid_of"
+    t.boolean  "got_rid_of",        default: false
   end
 
   create_table "users", force: :cascade do |t|
